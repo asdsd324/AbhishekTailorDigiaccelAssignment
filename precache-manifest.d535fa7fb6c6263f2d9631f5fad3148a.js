ebdea5952c848d8a93c116ac1482a69ee3e0072f
Code Sandbox <hello@codesandbox.io>
2023-02-07 04:53:49 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d535fa7fb6c6263f2d9631f5fad3148a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d38c181153499a545e933a3beeb2367",
+    "url": "https://asdsd324.github.io/csb-nc6v92/index.html"
+  },
+  {
+    "revision": "1c2a5dbd56185d0c3773",
+    "url": "https://asdsd324.github.io/csb-nc6v92/static/css/main.6663d4f0.chunk.css"
+  },
+  {
+    "revision": "634eb362dbe07e9bd9c8",
+    "url": "https://asdsd324.github.io/csb-nc6v92/static/js/2.2297a243.chunk.js"
+  },
+  {
+    "revision": "1c2a5dbd56185d0c3773",
+    "url": "https://asdsd324.github.io/csb-nc6v92/static/js/main.a7ef2c6d.chunk.js"
+  },
+  {
+    "revision": "cd76ca7514755a2eb425",
+    "url": "https://asdsd324.github.io/csb-nc6v92/static/js/runtime~main.97c6b95f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://asdsd324.github.io/csb-nc6v92/precache-manifest.d535fa7fb6c6263f2d9631f5fad3148a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://asdsd324.github.io/csb-nc6v92/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
